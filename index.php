1acc92fa652c7ff64e24fea21b16027005f28f0a
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-24 15:46:49 +0100

    ADD CDN Axios

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/08d5002177.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/vue@3"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link rel="stylesheet" href="style.css">
     <title>Vue To Do List</title>
 </head>
