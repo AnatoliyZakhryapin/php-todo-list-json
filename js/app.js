f877981e442b4f7f0c5256fb5074f4a094b57353
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-11-24 16:49:29 +0100

    Update function storeNewTask

--- js/app.js
+++ js/app.js
@@ -13,6 +13,21 @@ createApp({
         },
         storeNewTodo(){
             console.log(this.newTodo)
+
+            const data = {
+				newTodo: this.newTodo,
+			}
+
+            axios
+				.post('store.php', data, {
+					headers: {
+						'Content-Type': 'multipart/form-data',
+					},
+				})
+				.then((res) => {					console.log(res.data)
+					this.todos = res.data.todos
+					this.newTodo = ''
+				})
         },
         isCompliteTask(object){
             console.log("iniziale", object)
